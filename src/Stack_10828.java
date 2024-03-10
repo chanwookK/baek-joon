242aea5dad15b763d55cce9a653d8697fa3fe956
kangchanwook <kan0202@naver.com>
2024-03-10 22:48:21 +0900

    [BackJoon] Stack_10828

---
+++ src/Stack_10828.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Stack_10828 {
+    public static void main(String[] args) throws IOException {
+
+        //stack 생성
+        Stack<Integer> stack = new Stack<>();
+
+        //read buffer 입력버퍼
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        //명령의 갯수 개산
+        int n = Integer.parseInt(br.readLine());
+
+        int status;
+
+        //명령 처리 구문
+        for(int i = 0; i < n; i++) {
+
+            //push 명령어 구분
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            if(st.countTokens() == 2){
+                st.nextToken();
+                int pushNum = Integer.parseInt(st.nextToken());
+                stack.push(pushNum);
+
+            }
+            else{
+                String instruction = st.nextToken();
+
+                status = 0;
+                switch (instruction){
+                    case "pop":
+                        if(stack.empty()) {
+                            status = -1;
+                            break;
+                        }
+                        status = stack.pop();
+                        break;
+                    case "size":
+                        status = stack.size();
+                        break;
+                    case "empty":
+                        status = stack.empty()? 1:0;
+                        break;
+                    case "top":
+                        if(stack.empty()){
+                            status = -1;
+                            break;
+                        }
+
+                        status = stack.peek();
+                        break;
+
+                }
+
+                bw.write(String.valueOf(status));
+                bw.newLine();
+                bw.flush();
+
+            }
+
+        }
+        bw.close();
+        br.close();
+
+    }
+}
